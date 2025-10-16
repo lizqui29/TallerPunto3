15b7b09bd730a207220bfe4c6e2b2518bd3f5661
lizqui29 <lizeth_isa.quinones@uao.edu.co>
2025-10-16 12:47:15 -0500

    Se ha creado el proyecto sobre las cuentas de crédito y sus respectivas clases en la carpeta Modelo.

---
+++ src/main/java/com/uao/bancofinanciero/BancoFinanciero.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.bancofinanciero;
+
+
+import javax.swing.JOptionPane;
+import Modelo.Cuentas;
+import Modelo.Calculadora;
+import Modelo.CuentaAhorros;
+import Modelo.CuentaCorriente;
+
+/**
+ *
+ * @author Usuario
+ */
+public class BancoFinanciero {
+    
+    
+
+   
+}
